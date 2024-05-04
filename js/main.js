e1db66d14726fe9b3cf7fdf13467bdf6a53f303f
TomasParavic <142460872+TomasParavic@users.noreply.github.com>
2024-05-04 19:04:13 -0300

    Llamamos a los 100 pokemones con nombre y numero

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+const listaPokemon = document.querySelector("#listaPokemon");
+const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+
+//recorremos hasta 100 
+for (let i = 1; i <= 100; i++) {
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then(data => mostrarPokemon(data))
+}
+
+function mostrarPokemon(poke) {
+
+    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+    tipos = tipos.join('');
+
+    let pokeId = poke.id.toString();
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML = `
+        <p class="pokemon-id-back">#${pokeId}</p>
+        <div class="pokemon-imagen">
+            <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
+        </div>
+        <div class="pokemon-info">
+            <div class="nombre-contenedor">
+                <p class="pokemon-id">#${pokeId}</p>
+                <h2 class="pokemon-nombre">${poke.name}</h2>
+        </div>
+    `;
+    listaPokemon.append(div);
+}
